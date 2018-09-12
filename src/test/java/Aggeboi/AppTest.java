42de58bd22107716192222e468ed288781813874
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2018-09-12 10:39:19 +0200

    Added one method with test

--- src/test/java/Aggeboi/AppTest.java
+++ src/test/java/Aggeboi/AppTest.java
@@ -1,5 +1,6 @@
 package Aggeboi;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void oneTest(){
+        App app = new App();
+        assertEquals(1, app.one());
     }
 }
